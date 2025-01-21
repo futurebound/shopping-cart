9360c55a23bd850bb5b795514e755dd2470802a6
futurebound <mitchellm1618@gmail.com>
2025-01-21 11:39:17 -0800

    fix: apply Prettier to jsx files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className="flex h-24 items-center justify-between bg-slate-500 px-4 text-white">
+    <nav className='flex h-24 items-center justify-between bg-slate-500 px-4 text-white'>
       <div>
-        <Link to="/">ShopApp</Link>
+        <Link to='/'>ShopApp</Link>
       </div>
-      <ul className="flex">
-        <li className="p-4">
-          <Link to="/">Home</Link>
+      <ul className='flex'>
+        <li className='p-4'>
+          <Link to='/'>Home</Link>
         </li>
-        <li className="p-4">
-          <Link to="/shop">Shop</Link>
+        <li className='p-4'>
+          <Link to='/shop'>Shop</Link>
         </li>
       </ul>
       <div>CART</div>
     </nav>
-  );
+  )
 }
