71900bea01e24c9c717ccd9d2c1ce70e33704b87
futurebound <mitchellm1618@gmail.com>
2025-01-21 11:22:59 -0800

    feat: NavBar with links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./index.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <h1 className="text-red-500">Shopping Cart</h1>
     </>
   );
---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import App from "./App";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "profile/:name",
+  //   element: <Profile />,
+  // },
+];
+
+export default routes;
